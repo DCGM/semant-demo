8df2deac8d1be926578b0cffc34dfcdf0ecea433
ArgonOutPlay <argonoutplay@gmail.com>
2025-12-04 19:45:54 +0100

    move rag routes from main to rag_routes.py

--- semant_demo_backend/semant_demo/routes/__init__.py
+++ semant_demo_backend/semant_demo/routes/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from .tag_routes import exp_router as tag_router
 from .user_collection_routes import exp_router as usr_collection_router
+from .rag_routes import exp_router as rag_router
 
 export_router = APIRouter()
 export_router.include_router(tag_router)
 export_router.include_router(usr_collection_router)
+export_router.include_router(rag_router)
 
 __all__ = ["export_router"]
