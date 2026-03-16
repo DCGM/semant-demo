0c067c4ef82f7d2a364504b194b8ac3c451b5876
Marek Sucharda <m.sucharda@email.cz>
2026-03-16 10:18:43 +0100

    add demo page for showing an example usage

--- semant_demo_frontend/src/router/routes.ts
+++ semant_demo_frontend/src/router/routes.ts
@@ -34,6 +34,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/AboutPage.vue') }]
   },
+  {
+    path: '/demo/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/DemoPage.vue') }]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
