e2b77c36124663993b93b03ebc09891e7bccae7a
ArgonOutPlay <argonoutplay@gmail.com>
2026-03-22 18:02:15 +0100

    remove dead code from incremental rag

--- semant_demo_backend/semant_demo/rag/incremental_rag.py
+++ semant_demo_backend/semant_demo/rag/incremental_rag.py
@@ -301,27 +301,6 @@ class IncrementalAdaptiveRagGenerator(BaseRag):
 
         counter_value = state.get("retrieval_iteration_counter", 0) + 1
 
-        GET_NEIGHBORS = False
-        if (GET_NEIGHBORS == True):
-            if (DEBUG_PRINT):
-                print(f"Getting neighbors")
-
-            start_chunks = all_chunks[:5]
-            expanded_chunks = []
-            seen_context = set()
-
-            for chunk in start_chunks:
-                expanded_text = await self.searcher.get_expanded_context(chunk.document, chunk.from_page)
-                if (expanded_text not in seen_context):
-                    new_chunk = chunk.model_copy()
-                    new_chunk.text = expanded_text
-                    expanded_chunks.append(new_chunk)
-                    seen_context.add(expanded_text)
-            
-            return  {"documents" : expanded_chunks[:5], 
-                 "retrieval_iteration_counter" : counter_value
-                 }
-
         return  {"documents" : all_chunks, 
                  "retrieval_iteration_counter" : counter_value
                  }
