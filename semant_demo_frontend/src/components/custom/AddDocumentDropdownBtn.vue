b8269044c2ee42a444e2d452de63a7b034f2a97a
Richard Juřica <richard.juricabtc@gmail.com>
2026-03-26 00:55:55 +0100

    Frontend: Implement AddDocumentDropdownBtn component

---
+++ semant_demo_frontend/src/components/custom/AddDocumentDropdownBtn.vue
@@ -0,0 +1,20 @@
+<template>
+  <BaseDropdownBtn
+    label="Add Document"
+    icon="add"
+    color="primary"
+    :items="[
+      { label: 'Add From Library', icon: 'library_add', action: () => emit('browseLibrary') },
+      { label: 'Create New Document', icon: 'description', action: () => emit('createDocument') },
+      { label: 'Upload Document', icon: 'upload_file', action: () => emit('uploadDocument') }
+    ]"
+  />
+</template>
+
+<script setup lang="ts">
+import BaseDropdownBtn from './BaseDropdownBtn.vue'
+
+const emit = defineEmits(
+  ['browseLibrary', 'createDocument', 'uploadDocument']
+)
+</script>
