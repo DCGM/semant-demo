68d3277bf0d2c16f1a5f2824f3cd6995619af714
Marek Sucharda <m.sucharda@email.cz>
2026-04-09 11:25:57 +0200

    remove unused

--- semant_demo_backend/semant_demo/routes/__init__.py
+++ semant_demo_backend/semant_demo/routes/__init__.py
@@ -3,15 +3,11 @@ from .tag_routes import exp_router as tag_router
 from .user_collection_routes import exp_router as usr_collection_router
 from .rag_routes import exp_router as rag_router
 from .summarizer_routes import exp_router as summarizer_router
-from .collections_routes import router as collections_router
-from .documents_routes import router as documents_router
 
 export_router = APIRouter()
 export_router.include_router(tag_router)
 export_router.include_router(usr_collection_router)
 export_router.include_router(rag_router)
 export_router.include_router(summarizer_router)
-export_router.include_router(collections_router)
-export_router.include_router(documents_router)
 
 __all__ = ["export_router"]
