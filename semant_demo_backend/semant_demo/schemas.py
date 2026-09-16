c087d8606d46223f57e2899c4f58e436ebf9fbb9
Richard Juřica <richard.juricabtc@gmail.com>
2026-09-16 13:28:48 +0200

    cleaning schemas from deprecated tagging models

--- semant_demo_backend/semant_demo/schemas.py
+++ semant_demo_backend/semant_demo/schemas.py
@@ -163,16 +163,11 @@ class FilteredChunksByTags(BaseModel):
     automatic_tags_ids: list[str]
 
 
-class FilterChunksByTagsResponse(BaseModel):
-    chunkTags: list[FilteredChunksByTags]
-
-
 class FilterChunksByTagsRequest(BaseModel):
     chunkIds: list[str]
     tagIds: list[str]
     positive: bool
     automatic: bool
-
 
 class SearchResponse(BaseModel):
     results: list[TextChunkWithDocument]
@@ -324,7 +319,6 @@ class CollectionNames(BaseModel):
     span_collection_name: str
     user_collection_link_name: str
     tag_to_user_collection_link_name: str
-    span_collection_name: str
 
 # Tagging configuration
 
@@ -333,52 +327,6 @@ class TaggingConfigParams(BaseModel):
     model_type: APIType
     model_name: str
     temperature: float = 1.0
-
-
-class TaggingConfig(BaseModel):
-    name: str
-    description: str
-    class_name: str
-    prompt_template: str
-    params: TaggingConfigParams
-
-
-class GetConfigsResponse(BaseModel):
-    configs: list[TaggingConfig]
-
-# tagging task
-
-
-class TagStartResponse(BaseModel):
-    job_started: bool
-    task_id: str
-    message: str
-
-
-class TagReqTemplate(BaseModel):
-    tag_name: str  # name of the tag
-    tag_shorthand: str  # shorthand for the name
-    tag_color: str  # color assigned to the tag
-    tag_pictogram: str  # image
-    tag_definition: str  # description of the tag
-    tag_examples: list[str]  # list of examples what should be tagged
-    collection_name: str
-
-
-class TaggingTaskReqTemplate(BaseModel):
-    tag_name: str  # name of the tag
-    tag_shorthand: str  # shorthand for the name
-    tag_color: str  # color assigned to the tag
-    tag_pictogram: str  # image
-    tag_definition: str  # description of the tag
-    tag_examples: list[str]  # list of examples what should be tagged
-    collection_name: str
-    task_config: TaggingConfig
-
-
-class TagResponse(BaseModel):
-    texts: list[str]
-    tags: list[str]
 
 class TagType(str, Enum):
     positive = "positive"
@@ -395,88 +343,7 @@ class TagData(BaseModel):
     collection_name: str
     tag_uuid: uuid.UUID | None
 
-
-class TagTasksResponse(BaseModel):
-    taskIDs: list[uuid.UUID]
-
-
-class CancelTaskResponse(BaseModel):
-    message: str
-    taskCanceled: bool
-
-
-class GetTagsResponse(BaseModel):
-    tags_lst: list[TagData]
-
-
-class GetTaggedChunksReq(BaseModel):
-    tag_uuids: list[uuid.UUID]
-    tag_type: TagType
-
-
-class RemoveTagReq(BaseModel):
-    tag_uuids: list[uuid.UUID]
-
-
-class TaggedChunks(BaseModel):
-    tag_uuid: uuid.UUID  # uuid of a tag selected in UI and belonging to the text chunk
-    text_chunk: str  # actual text chunk
-    chunk_id: str  # to apply changes later
-    # send collection name of the chunk for faster manipulation later
-    chunk_collection_name: str
-
-
-class GetTaggedChunksResponse(BaseModel):
-    # list of pairs text chunk and id belonging to it
-    chunks_with_tags: list[TaggedChunks]
-
-
-# approves/disapproves or creates a new span in database
-class ApproveTagReq(BaseModel):
-    collectionID: str
-    chunkID: str
-    tagID: str
-    spanID: str | None = None
-    start: int | None = None
-    end: int | None = None
-
-class ApproveTagResponse(BaseModel):
-    successful: bool
-
-
-class RemoveTagsResponse(BaseModel):
-    successful: bool
-
-# User collection
-
-
-class UserCollectionReqTemplate(BaseModel):
-    collection_name: str  # name of the collection
-    user_id: str  # user id
-
-
-class Chunk2CollectionReq(BaseModel):
-    collectionId: str  # id of the collection
-    chunkId: str  # chunk id
-
-
-class CollectionChunks(BaseModel):
-    text_chunk: str  # actual text chunk
-    chunk_id: str  # to apply changes later
-
-
-class GetCollectionChunksResponse(BaseModel):
-    # list of pairs text chunk and id belonging to it
-    chunks_of_collection: list[CollectionChunks]
-
 # TagSpans
-
-
-class SpanStoreMode(str, Enum):
-    embedded = "embedded"
-    separate = "separate"
-    both = "both"
-
 
 class SpanType(str, Enum):
     pos = "pos"
@@ -578,39 +445,6 @@ class BestTagProposalResponse(BaseModel):
 
 # Task Model
 TasksBase = declarative_base()
-
-
-class Task(TasksBase):
-    __tablename__ = "tasks"
-    # 36 is max number of chars in uuid
-    taskId = Column(String(36), primary_key=True)
-    # PENDING|RUNNING|COMPLETED|FAILED
-    status = Column(String(20), default="PENDING")
-    result = Column(JSON, nullable=True)
-    all_texts_count = Column(Integer, nullable=True)
-    processed_count = Column(Integer, nullable=True)
-    collection_name = Column(String, nullable=True)
-    tag_id = Column(String(36))  # 36 is max number of chars in uuid
-    tag_processing_data = Column(JSON, nullable=True)
-    time_updated = Column(DateTime(timezone=True),
-                          onupdate=funcs.now())  # store updated time for loading tasks sorted by time updated
-    task_name = Column(String, nullable=True)
-
-
-tag_class = {
-    "class": "Tag",
-    "properties": [
-        {"name": "tag_name", "dataType": ["string"]},
-        {"name": "tag_shorthand", "dataType": ["string"]},
-        {"name": "tag_color", "dataType": ["string"]},
-        {"name": "tag_pictogram", "dataType": ["string"]},
-        {"name": "tag_definition", "dataType": ["text"]},
-        {"name": "tag_examples", "dataType": ["text[]"]},
-        {"name": "collection_name", "dataType": ["string"]}
-    ]
-}
-
-
 class RagUserFeedback(TasksBase):
     __tablename__ = "rag_user_feedback"
     id = Column(Integer, primary_key=True, autoincrement=True)
